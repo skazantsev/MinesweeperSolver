fef6e2dcab33d9ba577ed41498f5c59a09a89f71
Sergey Kazantsev <sergey.kazantsev1@gmail.com>
2015-05-23 02:42:16 +0300

    Moved game interaction logic to a new component to make MinesweeperBot just an orchestrator

---
+++ src/MinesweeperSolver/Domain/GameInteractor.cs
@@ -0,0 +1,85 @@
+﻿using MinesweeperSolver.Common.BclExtensions;
+using MinesweeperSolver.Common.Enums;
+using MinesweeperSolver.Common.Helpers;
+using MinesweeperSolver.Common.WinApiConstants;
+using MinesweeperSolver.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MinesweeperSolver.Domain
+{
+    public class GameInteractor
+    {
+        private static readonly KeyValueList<ColorPoint, CellState> CellStateDeterminationList = new KeyValueList<ColorPoint, CellState>
+        {
+            {ColorPoint.Create(c => Coords.ColToCoord(c), r => Coords.RowToCoord(r), Color.FromArgb(255, 0, 0)), CellState.Boom },
+
+            {ColorPoint.Create(c => Coords.ColToCoord(c, 7), r => Coords.RowToCoord(r, 8), Color.FromArgb(0, 0, 255)), CellState.One },
+
+            {ColorPoint.Create(c => Coords.ColToCoord(c, 7), r => Coords.RowToCoord(r, 8), Color.FromArgb(0, 128, 0)), CellState.Two },
+
+            {ColorPoint.Create(c => Coords.ColToCoord(c, 10), r => Coords.RowToCoord(r, 8), Color.FromArgb(255, 0, 0)), CellState.Three },
+
+            {ColorPoint.Create(c => Coords.ColToCoord(c, 10), r => Coords.RowToCoord(r, 8), Color.FromArgb(0, 0, 128)), CellState.Four },
+
+            {ColorPoint.Create(c => Coords.ColToCoord(c, 10), r => Coords.RowToCoord(r, 8), Color.FromArgb(128, 0, 0)), CellState.Five },
+
+            {ColorPoint.Create(c => Coords.ColToCoord(c, 10), r => Coords.RowToCoord(r, 8), Color.FromArgb(0, 128, 128)), CellState.Six },
+
+            {ColorPoint.Create(c => Coords.ColToCoord(c, 3), r => Coords.RowToCoord(r, 2), Color.FromArgb(0, 0, 0)), CellState.Seven },
+
+            {ColorPoint.Create(c => Coords.ColToCoord(c, 10), r => Coords.RowToCoord(r, 8), Color.FromArgb(128, 128, 128)), CellState.Eight },
+
+            {ColorPoint.Create(c => Coords.ColToCoord(c), r => Coords.RowToCoord(r), Color.FromArgb(192, 192, 192)), CellState.Empty }
+        };
+
+        private readonly IntPtr _minesweeperWindow;
+
+        public GameInteractor(IEnumerable<string> gameWindowNames)
+        {
+            _minesweeperWindow = GetMinesweeperWindowPtr(gameWindowNames);
+        }
+
+        public void NewGame()
+        {
+            WinApiHelper.PostMessage(_minesweeperWindow, WindowMessages.WM_KEYDOWN, (IntPtr)Keys.F2, IntPtr.Zero);
+        }
+
+        public CellState GetCellState(int row, int col)
+        {
+            var cellState =
+                CellStateDeterminationList
+                    .Where(x => x.Key.Color == WinApiHelper.GetColor(_minesweeperWindow, x.Key.XPointGetter(col), x.Key.YPointGetter(row)))
+                    .Select(x => x.Value)
+                    .Cast<CellState?>()
+                    .FirstOrDefault();
+
+            return cellState != null ? cellState.Value : CellState.Unknown;
+        }
+
+        public void TickMine(int row, int col)
+        {
+            WinApiHelper.MouseClick(_minesweeperWindow, MouseButton.Right, Coords.ColToCoord(col), Coords.RowToCoord(row));
+        }
+
+        public void OpenCell(int row, int col)
+        {
+            WinApiHelper.MouseClick(_minesweeperWindow, MouseButton.Left, Coords.ColToCoord(col), Coords.RowToCoord(row));
+        }
+
+        private static IntPtr GetMinesweeperWindowPtr(IEnumerable<string> gameWindowNames)
+        {
+            foreach (var wndName in gameWindowNames)
+            {
+                var hWnd = WinApiHelper.FindWindow(null, wndName);
+                if (hWnd != IntPtr.Zero)
+                    return hWnd;
+            }
+
+            throw new WindowCannotBeFoundException();
+        }
+    }
+}
--- src/MinesweeperSolver/Domain/MinesweeperBot.cs
+++ src/MinesweeperSolver/Domain/MinesweeperBot.cs
@@ -1,57 +1,26 @@
-﻿using System.Drawing;
-using MinesweeperSolver.Common.BclExtensions;
+﻿using MinesweeperSolver.Common.BclExtensions;
 using MinesweeperSolver.Common.Constants;
-using MinesweeperSolver.Common.Enums;
 using MinesweeperSolver.Common.Events;
-using MinesweeperSolver.Common.Helpers;
-using MinesweeperSolver.Common.WinApiConstants;
 using MinesweeperSolver.Domain.Events;
-using MinesweeperSolver.Domain.Exceptions;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace MinesweeperSolver.Domain
 {
     public class MinesweeperBot
     {
-        private static readonly KeyValueList<ColorPoint, CellState> CellStateDeterminationList = new KeyValueList<ColorPoint, CellState>
-        {
-            {ColorPoint.Create(c => Coords.ColToCoord(c), r => Coords.RowToCoord(r), Color.FromArgb(255, 0, 0)), CellState.Boom },
-
-            {ColorPoint.Create(c => Coords.ColToCoord(c, 7), r => Coords.RowToCoord(r, 8), Color.FromArgb(0, 0, 255)), CellState.One },
-
-            {ColorPoint.Create(c => Coords.ColToCoord(c, 7), r => Coords.RowToCoord(r, 8), Color.FromArgb(0, 128, 0)), CellState.Two },
-
-            {ColorPoint.Create(c => Coords.ColToCoord(c, 10), r => Coords.RowToCoord(r, 8), Color.FromArgb(255, 0, 0)), CellState.Three },
-
-            {ColorPoint.Create(c => Coords.ColToCoord(c, 10), r => Coords.RowToCoord(r, 8), Color.FromArgb(0, 0, 128)), CellState.Four },
-
-            {ColorPoint.Create(c => Coords.ColToCoord(c, 10), r => Coords.RowToCoord(r, 8), Color.FromArgb(128, 0, 0)), CellState.Five },
-
-            {ColorPoint.Create(c => Coords.ColToCoord(c, 10), r => Coords.RowToCoord(r, 8), Color.FromArgb(0, 128, 128)), CellState.Six },
-
-            {ColorPoint.Create(c => Coords.ColToCoord(c, 3), r => Coords.RowToCoord(r, 2), Color.FromArgb(0, 0, 0)), CellState.Seven },
-
-            {ColorPoint.Create(c => Coords.ColToCoord(c, 10), r => Coords.RowToCoord(r, 8), Color.FromArgb(128, 128, 128)), CellState.Eight },
-
-            {ColorPoint.Create(c => Coords.ColToCoord(c), r => Coords.RowToCoord(r), Color.FromArgb(192, 192, 192)), CellState.Empty }
-        };
-
-        private string[] _gameWindowNames;
-
-        private IntPtr _minesweeperWindow;
+        private GameInteractor _gameInteractor;
         private Map _map;
         private GameSolver _solver;
 
         public bool IsStarted { get; private set; }
 
-        public void Start(GameOptions gameOptions, string[] gameWindowNames)
+        public void Start(GameOptions gameOptions, IEnumerable<string> gameWindowNames)
         {
-            _gameWindowNames = gameWindowNames;
-            _minesweeperWindow = GetMinesweeperWindowPtr();
+            _gameInteractor = new GameInteractor(gameWindowNames);
             _map = new Map(gameOptions.Height, gameOptions.Width);
             _solver = new GameSolver(_map, gameOptions.Mines);
             _solver.MineHasBeenFound += MineHasBeenFoundHandler;
@@ -60,13 +29,13 @@ namespace MinesweeperSolver.Domain
             _solver.GameLost += GameLostHandler;
 
             IsStarted = true;
-            WinApiHelper.PostMessage(_minesweeperWindow, WindowMessages.WM_KEYDOWN, (IntPtr)Keys.F2, IntPtr.Zero);
+            _gameInteractor.NewGame();
             Task.Factory.StartNew(BeginMapPollingLoop);
         }
 
         public void Stop()
         {
-            WinApiHelper.PostMessage(_minesweeperWindow, WindowMessages.WM_KEYDOWN, (IntPtr)Keys.F2, IntPtr.Zero);
+            _gameInteractor.NewGame();
             Complete();
         }
 
@@ -82,21 +51,6 @@ namespace MinesweeperSolver.Domain
             _solver.NextCellHasBeenFound -= NextCellHasBeenFoundHandler;
             _solver.GameSolved -= GameSolvedHandler;
             _solver.GameLost -= GameLostHandler;
-            _solver = null;
-            _map = null;
-            _minesweeperWindow = IntPtr.Zero;
-        }
-
-        private IntPtr GetMinesweeperWindowPtr()
-        {
-            foreach (var wndName in _gameWindowNames)
-            {
-                var hWnd = WinApiHelper.FindWindow(null, wndName);
-                if (hWnd != IntPtr.Zero)
-                    return hWnd;
-            }
-
-            throw new WindowCannotBeFoundException();
         }
 
         private void BeginMapPollingLoop()
@@ -122,22 +76,20 @@ namespace MinesweeperSolver.Domain
 
         private void UpdateMapState()
         {
-            _map.GetCells().Where(x => x.State == CellState.Unknown).ForEach(x => x.UpdateState(GetCellState(x.Row, x.Col)));
+            _map.GetCells()
+                .Where(x => x.State == CellState.Unknown)
+                .ForEach(x => x.UpdateState(_gameInteractor.GetCellState(x.Row, x.Col)));
         }
 
         private void MineHasBeenFoundHandler(object sender, GameSolver.CellLocationEventArgs e)
         {
             _map.GetCellAt(e.Row, e.Col).UpdateState(CellState.Mine);
-            var coordX = Coords.ColToCoord(e.Col);
-            var coordY = Coords.RowToCoord(e.Row);
-            WinApiHelper.MouseClick(_minesweeperWindow, MouseButton.Right, coordX, coordY);
+            _gameInteractor.TickMine(e.Row, e.Col);
         }
 
         private void NextCellHasBeenFoundHandler(object sender, GameSolver.CellLocationEventArgs e)
         {
-            var coordX = Coords.ColToCoord(e.Col);
-            var coordY = Coords.RowToCoord(e.Row);
-            WinApiHelper.MouseClick(_minesweeperWindow, MouseButton.Left, coordX, coordY);
+            _gameInteractor.OpenCell(e.Row, e.Col);
         }
 
         private void GameSolvedHandler(object sender, EventArgs e)
@@ -150,18 +102,6 @@ namespace MinesweeperSolver.Domain
         {
             Complete();
             DomainEvents.Raise(new GameIsLost());
-        }
-
-        private CellState GetCellState(int row, int col)
-        {
-            var cellState =
-                CellStateDeterminationList
-                    .Where(x => x.Key.Color == WinApiHelper.GetColor(_minesweeperWindow, x.Key.XPointGetter(col), x.Key.YPointGetter(row)))
-                    .Select(x => x.Value)
-                    .Cast<CellState?>()
-                    .FirstOrDefault();
-
-            return cellState != null ? cellState.Value : CellState.Unknown;
         }
     }
 }
